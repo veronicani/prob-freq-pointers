adc1829a3b9269c054b80afb6bb65d5eb1e665ba
Teagan McClintock <teagan.mcclintock@gmail.com>
2023-11-10 11:33:52 -0500

    Solved average-pair.js and added docstring to getFrequencyCounter function in can-construct-word.js

--- average-pair.js
+++ average-pair.js
@@ -1,4 +1,37 @@
+"use strict"
+
 // add whatever parameters you deem necessary & write docstring
-function averagePair() {
+
+/**
+ * averagePair takes a sorted array of numbers and a provided number
+ *  It returns true if there exists at least one pair of numbers in the array
+ *  whose average is the provided number, and false otherwise
+ *
+ */
+function averagePair(nums, targetAvg) {
+  //create indices at either end of nums
+  // do next 4 lines until start index > end index
+  // check if sum of numbers at those positions equals targetAvg * 2
+  // if so, return true
+  // if it was greater, move end index down 1
+  // if it was less, move start index up 1
+  // return false if we've reached the end of the loop
+
+  const targetSum = targetAvg * 2;
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left < right){
+    if (nums[left] + nums[right] === targetSum){
+      return true;
+    }
+    else if (nums[left] + nums[right] > targetSum){
+      right--;
+    }
+    else {
+      left++;
+    }
+  }
+  return false;
 }
 
--- can-construct-word.js
+++ can-construct-word.js
@@ -1,7 +1,7 @@
 "use strict"
 
 // add whatever parameters you deem necessary & write doc comment
-/** accepts 2 strings: word and letters 
+/** accepts 2 strings: word and letters
  * return true if word can be built with letters -- false if not
  * inputs will only be lowercase letters
 */
@@ -9,7 +9,7 @@ function canConstructWord(word, letters) {
   //count the freq of chars in word and letters
   const wordFreq = getFrequencyCounter(word);
   const lettersFreq = getFrequencyCounter(letters);
-  
+
   //check each char in the word
   for (const char in wordFreq) {
     //if the val of the char is greater than val of char in letters
@@ -23,12 +23,17 @@ function canConstructWord(word, letters) {
   return true;
 }
 
-function getFrequencyCounter(items) {  
+
+/** takes an iterable and returns a frequency counter object with each
+ *  value in the input as a key and the occurences of that value in the input
+ *  as the key's value
+*/
+function getFrequencyCounter(items) {
   const freqs = {};
 
   for (const item of items) {
     const curr = freqs[item] || 0;
-    freqs[item] = curr + 1;  
+    freqs[item] = curr + 1;
   }
 
   return freqs;
