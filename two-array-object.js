3e969236bcec0ffc17f2b078a82a6d6da96098cd
Veronica Ni <vni1294@gmail.com>
2023-11-10 19:47:01 -0500

    solved two-array-objs

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,24 @@
 // add whatever parameters you deem necessary & write docstring
-function twoArrayObject() {
+/** Recieves two arrays, of different lengths (keys and values)
+ * Return object created from keys and values
+ * If there are more keys than values, extra keys should have value of null
+*/
+function twoArrayObject(keys, vals) {
+  const keysToVals = {};
+  //loop over keys
+  for (let i = 0; i < keys.length; i++) {
+    //for each key, if val at the same idx is undefined
+    if (vals[i] === undefined) {
+      //map 'null' as the value to the key in obj  
+      keysToVals[keys[i]] = null;
+    } else {
+      //add prop of key to object with value of val at same idx
+      keysToVals[keys[i]] = vals[i];
+    } 
+
+  }
+
+  return keysToVals;
 }
+
+
